1491f05e6b58bc3cd575cbc76fe55bb3df0980de
Eric Song <ericksong91@gmail.com>
2023-10-19 13:39:35 -0400

    Adding navbar skeleton

--- client/src/top/Navbar.js
+++ client/src/top/Navbar.js
@@ -4,12 +4,15 @@ function Navbar() {
     return (
         <nav class="w-full fixed top-0 bg-red-400">
             <div class="container mx-auto py-5 flex justify-between">
+                <div>
+                    <img src="../img/logo.png" alt="logo" />
+                </div>
                 <span>1</span>
                 <span>2</span>
                 <span>3</span>
             </div>
         </nav>
-    )
-}
+    );
+};
 
 export default Navbar
